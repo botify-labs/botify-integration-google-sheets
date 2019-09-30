48ed34ef0784c2732708dbd8b997ee48adadbccd
Yves Bastide <yves@botify.com>
2019-09-30 16:11:31 +0200

    Misc. fixes/style changes/etc.
    * commonize extractRowKeys
    * cleanup code: no semicolons and so

--- src/BOTIFY_ANALYSIS_LIST_URLS.js
+++ src/BOTIFY_ANALYSIS_LIST_URLS.js
@@ -1,3 +1,4 @@
+// noinspection JSUnusedGlobalSymbols
 /**
  * Return the requested fields of a given URL
  * @param {String} username Username of the project owner
@@ -23,7 +24,7 @@ function BOTIFY_ANALYSIS_LIST_URLS(
   page,
   displayTotal
 ) {
-  var apiToken = getTokenFromProperties();
+  var apiToken = getTokenFromProperties()
   // Support old token format
   // Old signature was BOTIFY_ANALYSIS_LIST_URLS(apiToken, username, projectSlug, analysisSlug, fields, (filter), (sort), (size), (page), (displayTotal))
   if (
@@ -32,49 +33,49 @@ function BOTIFY_ANALYSIS_LIST_URLS(
   ) {
     // Only override the token if not present
     if (!apiToken) {
-      apiToken = arguments[0];
+      apiToken = arguments[0]
     }
     // Override parameters in any case so they are correct
-    username = arguments[1];
-    projectSlug = arguments[2];
-    analysisSlug = arguments[3];
-    fields = arguments[4];
-    filter = arguments[5];
-    sort = arguments[6];
-    size = arguments[7];
-    page = arguments[8];
-    displayTotal = arguments[9];
+    username = arguments[1]
+    projectSlug = arguments[2]
+    analysisSlug = arguments[3]
+    fields = arguments[4]
+    filter = arguments[5]
+    sort = arguments[6]
+    size = arguments[7]
+    page = arguments[8]
+    displayTotal = arguments[9]
   }
 
   // PARAMS CHECKING
   if (!apiToken)
     throw new Error(
       "API Token is missing in the Addon configuration. Click on the Botify Addon item in the menu to add your token."
-    );
-  if (!username) throw new Error("username is missing in parameters");
-  if (!projectSlug) throw new Error("projectSlug is missing in parameters");
-  if (!analysisSlug) throw new Error("analysisSlug is missing in parameters");
-  if (!fields) throw new Error("fields list is missing in parameters");
-  if (typeof filter === "undefined") filter = "{}";
-  if (typeof sort === "undefined") sort = "[]";
-  if (typeof size === "undefined") size = 100;
+    )
+  if (!username) throw new Error("username is missing in parameters")
+  if (!projectSlug) throw new Error("projectSlug is missing in parameters")
+  if (!analysisSlug) throw new Error("analysisSlug is missing in parameters")
+  if (!fields) throw new Error("fields list is missing in parameters")
+  if (typeof filter === "undefined") filter = "{}"
+  if (typeof sort === "undefined") sort = "[]"
+  if (typeof size === "undefined") size = 100
   if (typeof size > 1000)
-    throw new Error("size parameter must be between 1 and 1000");
-  if (typeof page === "undefined") page = 1;
-  if (typeof displayTotal === "undefined") displayTotal = false;
+    throw new Error("size parameter must be between 1 and 1000")
+  if (typeof page === "undefined") page = 1
+  if (typeof displayTotal === "undefined") displayTotal = false
 
-  var result = [];
+  var result = []
 
   // PREPARE INPUTS
   if (fields.map) {
-    fields = fields[0].filter(function(v) {
-      return !!v;
-    }); // remove empty fields
+    fields = fields[0].filter(function (v) {
+      return !!v
+    }) // remove empty fields
   } else {
-    fields = [fields];
+    fields = [fields]
   }
-  filter = JSON.parse(filter);
-  sort = JSON.parse(sort);
+  filter = JSON.parse(filter)
+  sort = JSON.parse(sort)
 
   // FETCHING API
   var apiurl =
@@ -87,7 +88,7 @@ function BOTIFY_ANALYSIS_LIST_URLS(
     "/urls?page=" +
     page +
     "&size=" +
-    size;
+    size
   var options = {
     method: "post",
     headers: {
@@ -100,43 +101,43 @@ function BOTIFY_ANALYSIS_LIST_URLS(
       filters: filter,
       sort: sort
     })
-  };
+  }
 
   var response = JSON.parse(
     UrlFetchApp.fetch(apiurl, options).getContentText()
-  );
+  )
 
   if (displayTotal) {
-    result.push(["Total Urls", response.count]);
+    result.push(["Total Urls", response.count])
   }
 
-  response.results.forEach(function(item) {
+  response.results.forEach(function (item) {
     // Get requested fields
-    var row = [];
-    fields.forEach(function(field) {
-      var val = get(item, field);
-      row.push(val);
-    });
-    result.push(row);
-  });
+    var row = []
+    fields.forEach(function (field) {
+      var val = get(item, field)
+      row.push(val)
+    })
+    result.push(row)
+  })
 
-  if (!result.length) return "No result";
+  if (!result.length) return "No result"
 
-  return result;
+  return result
 }
 
 function get(obj, path) {
-  path = path.split(".");
+  path = path.split(".")
   for (var i = 0; i < path.length; i++) {
-    obj = obj[path[i]];
+    obj = obj[path[i]]
     if (typeof obj === "undefined") {
-      return null;
+      return null
     }
   }
-  return isObject(obj) ? JSON.stringify(obj) : obj;
+  return isObject(obj) ? JSON.stringify(obj) : obj
 }
 
 function isObject(value) {
-  var type = typeof value;
-  return value != null && (type == "object" || type == "function");
+  var type = typeof value
+  return value != null && (type === "object" || type === "function")
 }
--- src/BOTIFY_PROJECT_AGGREGATE_URLS.js
+++ src/BOTIFY_PROJECT_AGGREGATE_URLS.js
@@ -1,3 +1,4 @@
+// noinspection JSUnusedGlobalSymbols
 /**
  * Return the result of an aggregation for latest project's analyses.
  * @param {String} username Username of the project owner
@@ -15,7 +16,7 @@ function BOTIFY_PROJECT_AGGREGATE_URLS(
   nbAnalyses,
   errors
 ) {
-  var apiToken = getTokenFromProperties();
+  var apiToken = getTokenFromProperties()
   // Support old token format
   // Old signature was BOTIFY_PROJECT_AGGREGATE_URLS(apiToken, username, projectSlug, urlsAggsQuery, (nbAnalyses))
   if (
@@ -24,13 +25,13 @@ function BOTIFY_PROJECT_AGGREGATE_URLS(
   ) {
     // Only override the token if not present
     if (!apiToken) {
-      apiToken = arguments[0];
+      apiToken = arguments[0]
     }
     // Override parameters in any case so they are correct
-    username = arguments[1];
-    projectSlug = arguments[2];
-    urlsAggsQuery = arguments[3];
-    nbAnalyses = arguments[4];
+    username = arguments[1]
+    projectSlug = arguments[2]
+    urlsAggsQuery = arguments[3]
+    nbAnalyses = arguments[4]
     errors = arguments[5]
   }
 
@@ -38,26 +39,26 @@ function BOTIFY_PROJECT_AGGREGATE_URLS(
   if (!apiToken)
     throw new Error(
       "API Token is missing in the Addon configuration. Click on the Botify Addon item in the menu to add your token."
-    );
-  if (!username) throw new Error("username is missing in parameters");
-  if (!projectSlug) throw new Error("projectSlug is missing in parameters");
-  if (!urlsAggsQuery) throw new Error("urlsAggsQuery is missing in parameters");
-  if (typeof nbAnalyses === "undefined") nbAnalyses = 5;
+    )
+  if (!username) throw new Error("username is missing in parameters")
+  if (!projectSlug) throw new Error("projectSlug is missing in parameters")
+  if (!urlsAggsQuery) throw new Error("urlsAggsQuery is missing in parameters")
+  if (typeof nbAnalyses === "undefined") nbAnalyses = 5
 
-  var result = [[]];
+  var result = [[]]
 
   // TEST urlsAggsQuery
-  urlsAggsQuery = JSON.parse(urlsAggsQuery);
+  urlsAggsQuery = JSON.parse(urlsAggsQuery)
   if (urlsAggsQuery.aggs.length > 1) {
-    throw new Error("ERROR: you cannot define more than 1 aggregation");
+    throw new Error("ERROR: you cannot define more than 1 aggregation")
   }
-  var requestAgg = urlsAggsQuery.aggs[0] || {};
+  var requestAgg = urlsAggsQuery.aggs[0] || {}
   if (
     requestAgg.group_by &&
     requestAgg.metrics &&
     requestAgg.metrics.length > 1
   ) {
-    throw new Error("ERROR: you can define only one metrics with groupbys");
+    throw new Error("ERROR: you can define only one metrics with groupbys")
   }
 
   // FETCHING API
@@ -66,9 +67,9 @@ function BOTIFY_PROJECT_AGGREGATE_URLS(
     username +
     "/" +
     projectSlug +
-    "/urls/aggs";
+    "/urls/aggs"
   if (nbAnalyses) {
-    url += "?nb_analyses=" + nbAnalyses;
+    url += "?nb_analyses=" + nbAnalyses
   }
   var options = {
     method: "post",
@@ -78,110 +79,93 @@ function BOTIFY_PROJECT_AGGREGATE_URLS(
       "X-Botify-Client": "google-sheets"
     },
     payload: JSON.stringify([urlsAggsQuery])
-  };
-  var responses = JSON.parse(UrlFetchApp.fetch(url, options).getContentText());
+  }
+  var responses = JSON.parse(UrlFetchApp.fetch(url, options).getContentText())
 
   // INSERT HEADERS
   if (requestAgg.group_by) {
     requestAgg.group_by.forEach(function (group) {
       if (group.range) {
         // Range group by
-        result[0].push(group.range.field);
+        result[0].push(group.range.field)
       } else {
         // Simple group by
-        result[0].push(group);
+        result[0].push(group)
       }
-    });
+    })
   } else {
-    result[0].push("Metric");
+    result[0].push("Metric")
     if (requestAgg.metrics) {
       requestAgg.metrics.forEach(function (metric, i) {
         if (typeof metric === "string") {
-          result[i + 1] = [metric.toUpperCase()];
+          result[i + 1] = [metric.toUpperCase()]
         } else {
-          var operation = Object.keys(metric)[0];
-          result[i + 1] = [operation.toUpperCase() + " " + metric[operation]];
+          var operation = Object.keys(metric)[0]
+          result[i + 1] = [operation.toUpperCase() + " " + metric[operation]]
         }
-      });
+      })
     } else {
       // Default metric is count
-      result[1] = ["count"];
+      result[1] = ["count"]
     }
   }
 
-  if (!responses[0]) return result;
-  var start = result[0].length;
+  if (!responses[0]) return result
+  var start = result[0].length
 
   // APPEND ROW RESULTS
-  var rowIds = [];
-  responses[0].reverse();
+  var rowIds = []
+  responses[0].reverse()
   responses[0].forEach(function (response) {
     // 1 response per analysis
-    var colIdx = result[0].push(response.analysis_slug) - 1; // Add analysis slug in first column
+    var colIdx = result[0].push(response.analysis_slug) - 1 // Add analysis slug in first column
     var groups =
       (response.data === undefined && []) ||
       (response.data.aggs[0] && response.data.aggs[0].groups) ||
-      response.data.aggs;
+      response.data.aggs
     groups.forEach(function (resultGroup) {
       // For each group by combination
       // Add group keys
+      var rowId = []
       if (resultGroup.key) {
-        var rowId = [];
-        resultGroup.key.forEach(function (key) {
-          if (
-            typeof key.from !== "undefined" ||
-            typeof key.to !== "undefined"
-          ) {
-            // Range group key
-            if (typeof key.from === "undefined") {
-              rowId.push("< " + key.to);
-            } else if (typeof key.to === "undefined") {
-              rowId.push(">= " + key.from);
-            } else {
-              rowId.push(key.from + " to " + key.to);
-            }
-          } else {
-            // Simple group key
-            rowId.push(key);
-          }
-        });
-        var rowIdx = rowIds.indexOf(rowId.join("#")) + 1;
+        rowId = extractRowKeys(resultGroup)
+        var rowIdx = rowIds.indexOf(rowId.join("#")) + 1
         if (!rowIdx) {
-          rowIdx = rowIds.push(rowId.join("#"));
-          result[rowIdx] = rowId;
+          rowIdx = rowIds.push(rowId.join("#"))
+          result[rowIdx] = rowId
         }
-        result[rowIdx][colIdx] = resultGroup.metrics[0];
+        result[rowIdx][colIdx] = resultGroup.metrics[0]
       } else {
         resultGroup.metrics.forEach(function (metric, i) {
-          result[i + 1][colIdx] = metric;
-        });
+          result[i + 1][colIdx] = metric
+        })
       }
-    });
-  });
+    })
+  })
 
   // fill with 0
   for (var i = 0; i < result.length; i++) {
     for (var j = 0; j < result[0].length; j++) {
       if (typeof result[i][j] === "undefined") {
-        result[i][j] = 0;
+        result[i][j] = 0
       }
     }
   }
 
   // Append errors
   if (errors) {
-    var errors_list = [];
-    errors_list.length = result[0].length;
-    var have_errors = false;
+    var errors_list = []
+    errors_list.length = result[0].length
+    var have_errors = false
     responses[0].forEach(function (response, i) {
       if (response.error) {
-        have_errors = true;
+        have_errors = true
         errors_list[start + i] = response.error.message
       }
-    });
+    })
     if (have_errors) {
       result.push(errors_list)
     }
   }
-  return result;
+  return result
 }
